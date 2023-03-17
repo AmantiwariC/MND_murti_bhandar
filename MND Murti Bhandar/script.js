f0c28ef6969b172a805a84055e074d0f221da4b0
AmantiwariC <116155132+AmantiwariC@users.noreply.github.com>
2023-03-17 14:53:19 +0530

    in progress

---
+++ MND Murti Bhandar/script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const navbar = document.getElementById('navbar');
+
+if(bar){
+
+     bar.addEventListener("click", () => {
+         navbar.classList.toggle("active");
+         });
+}
+if(close){
+
+    close.addEventListener("click", () => {
+        navbar.classList.remove("active");
+        });
+}
